ca2274040d848cd979f003c58a06c04067e8d47a
anild <anild@qburst.com>
2016-04-06 14:36:52 +0530

    licence added in every file

--- init.rb
+++ init.rb
@@ -1,5 +1,27 @@
+=begin
+
+Apache License
+
+Version 2.0, January 2004
+
+Copyright (c) 2016 QBurst Technologies Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, 
+software distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+either express or implied.
+
+See the License for the specific language governing permissions and limitations under the License.
+
+=end
+
 #
-# Initializer for the plugin to intialize appilciation level varaibles during the statup.
 # extend User class to have a one-to-many relationship with DeviceToken
 # extend Issue class to execute worker to send push notification when an issue has been reported or changed.
 #
